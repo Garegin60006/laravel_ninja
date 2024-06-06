4ec65e8fb01e82da57d2a2b5ca79f01efcc1fb09
Garik <garegin.joinunlock@gmail.com>
2024-06-06 21:15:46 +0400

    some thing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/pizzas', function () {
+    $pizza = [
+        'type' => 'hawaiian', 
+        'base' => 'cheesy crust',
+        'price' => 10
+      ];
+    return view('pizzas', $pizza);
+
+    // return ['name' => 'Pak', 'age' => 45];
+});
